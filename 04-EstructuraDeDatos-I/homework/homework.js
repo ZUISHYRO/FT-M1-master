ceed1f2febc073af4f9978411fc77ea13a44b859
AngelBernal <kirito8900@hotmail.com>
2022-11-11 05:18:23 -0700

    test modificado

--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -6,16 +6,7 @@ const { size } = require("@11ty/eleventy/src/TemplateCache");
 Definir las funciones recursivas nFactorial y nFibonacci.
 
 nFactorial(n) debe retornar el factorial de n sabiendo que, siendo n un número natural, su factorial (representado como n!) es el producto de n por todos los números naturales menores que él y mayores a 0. Ejemplo: 5! = 5 * 4 * 3 * 2 * 1
-
-nFibonacci(n) debe retornar el enésimo número de la secuencia de Fibonacci, tomando al 0 y al 1, respectivamente, como primer y segundo elementos de la misma, y sabiendo que cualquier elemento que se agregue a esta secuencia será el resultado de la suma del último elemento y el anterior.
-Ejemplo: nFibonacci(7) retornará 13, ya que 13 es el dígito que está en la posición 7 de la secuencia.
-
-Secuencia:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ... 
-
-
-Como ejercicio adicional y completamente opcional, al terminar de resolver este problema pueden intentar definir funciones que logren los mismos resultados pero de manera iterativa.
 */
-
 function nFactorial(n) {
   if (n === 0) {
      return 1;
@@ -23,6 +14,13 @@ function nFactorial(n) {
    return n * nFactorial(n-1);
   }
 }
+
+/*
+nFibonacci(n) debe retornar el enésimo número de la secuencia de Fibonacci, tomando al 0 y al 1, respectivamente, como primer y segundo elementos de la misma, y sabiendo que cualquier elemento que se agregue a esta secuencia será el resultado de la suma del último elemento y el anterior.
+Ejemplo: nFibonacci(7) retornará 13, ya que 13 es el dígito que está en la posición 7 de la secuencia.
+
+Secuencia:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ... */
+
 console.log(nFactorial(1));
 
 function nFibonacci(n) {
@@ -34,6 +32,12 @@ function nFibonacci(n) {
   
 }
 console.log(nFibonacci(9));
+
+/*
+Como ejercicio adicional y completamente opcional, al terminar de resolver este problema pueden intentar definir funciones que logren los mismos resultados pero de manera iterativa.
+*/
+
+
 /*
 Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde el primer elemento que ingresa es el primero que se quita. Definir los siguientes métodos:
   - enqueue: agrega un valor respetando el orden.
